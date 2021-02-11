94fd58a1c1cd8fc37d8d7758772ae092cae07832
Ruben Duarte (@kubuntu) <druidbran@gmail.com>
2021-02-11 19:03:25 +0000

    merge

--- src/org/academiadecodigo/rapunshells/Enemies/Soldier.java
+++ src/org/academiadecodigo/rapunshells/Enemies/Soldier.java
@@ -2,13 +2,34 @@ package org.academiadecodigo.rapunshells.Enemies;
 
 import org.academiadecodigo.rapunshells.Guns.Bullet;
 import org.academiadecodigo.rapunshells.Guns.Gun;
+import org.academiadecodigo.rapunshells.Window;
+import org.academiadecodigo.simplegraphics.graphics.Color;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
 
 public class Soldier extends Enemy {
+    private Rectangle soldierVisual;
+    private int charHeight = Window.getCelSizeY() * 6;
+    private int charWidth = Window.getCelSizeX() * 8;
+    private int charStartPointX = 1200;
+    private int charStartPointY = Window.getFLOORCOORD() - charHeight;
 
     public Soldier() {
         health = 100;
         gun = new Gun(10, 500, this);
+        soldierVisual = new Rectangle(charStartPointX,charStartPointY,charWidth,charHeight);
+        soldierVisual.draw();
+        soldierVisual.fill();
+        soldierVisual.setColor(Color.BLACK);
+        while (health>0) {
+            for (int i = 0; i < 10; i++) {
+                this.translate(1,0);
+                this.translate(1,0);
+                this.translate(1,0);
+                this.shoot();
+            }
+        }
     }
+
 
     @Override
     public void hit(Bullet bullet) {

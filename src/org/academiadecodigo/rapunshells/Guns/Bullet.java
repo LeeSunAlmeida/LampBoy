efdf2368e12fe67087385a9873b637f4e3a0f53d
Alexandre Sobral <alex.sobral35@gmail.com>
2021-02-13 16:57:20 +0000

    edited the name of lib directory

--- src/org/academiadecodigo/rapunshells/Guns/Bullet.java
+++ src/org/academiadecodigo/rapunshells/Guns/Bullet.java
@@ -47,7 +47,6 @@ public class Bullet implements Movable {
         if (bulletVisual.getX() >= enemy.getEnemyVisual().getX() && bulletVisual.getX() <= enemy.getEnemyVisual().getX() + enemy.getCharWidth()
                 && bulletVisual.getY() >= enemy.getEnemyVisual().getY() && (bulletVisual.getY() <= enemy.getEnemyVisual().getY() + enemy.getCharHeight())) {
             System.out.println("bullet touched enemy");
-            //bulletVisual.delete();  //quando a bala atinge o inimigo ela não continua a andar
             exist = false;
             enemy.hit(this);
             System.out.println(enemy.getHealth());
